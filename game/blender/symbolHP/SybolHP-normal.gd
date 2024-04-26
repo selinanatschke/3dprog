ee37f4c104187a49f3d34a0d58b3c04c24f512a7
Selina Natschke <natschke.selina@gmail.com>
2024-04-26 17:40:33 +0200

    implemented door opening functionality

--- game/blender/symbolHP/SybolHP-normal.gd
+++ game/blender/symbolHP/SybolHP-normal.gd
@@ -3,6 +3,8 @@ extends Spatial
 onready var hp_glowing: Node = get_parent().get_node("SybolHP-leuchtend")
 onready var door_object: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolHPnormal")
 onready var door_object_glowing: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolHPleuchtend")
+onready var door_object_glowing2: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolTOGleuchtend")
+onready var door: Node = get_node("/root/Main/MainRoom/Doorway/door")
 
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
@@ -13,4 +15,6 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 			door_object.visible = false
 			door_object_glowing.visible = true
 			
-			
+		if door_object_glowing.visible == true and door_object_glowing2.visible == true:
+			door.translation = Vector3(15.604, 4.083, 19.211)
+			door.rotation_degrees = Vector3(0, 80, 0)
--- game/blender/symbolTOG/SymbolToG-normal.gd
+++ game/blender/symbolTOG/SymbolToG-normal.gd
@@ -3,13 +3,22 @@ extends Spatial
 onready var object_to_hide: Node = get_parent().get_node("SymbolToG-leuchtend")
 onready var door_object: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolTOGnormal")
 onready var door_object_glowing: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolTOGleuchtend")
+onready var door_object_glowing2: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolHPleuchtend")
+onready var door: Node = get_node("/root/Main/MainRoom/Doorway/door")
+
 
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.pressed == true:
-			print("pressed mouse button")
+			print("pressed mouse button", door_object_glowing2.visible)
 			self.visible = false
 			object_to_hide.visible = true
 			door_object.visible = false
 			door_object_glowing.visible = true
+			
+			if door_object_glowing.visible and door_object_glowing2.visible:
+				print("auf", door_object_glowing2.visible)
+				door.translation = Vector3(16.604, 4.309, 20.711)
+				door.rotation_degrees = Vector3(0, 80, 0)
+
 			
