f3a70685ed460296fd5a3465bb2206730eeb83e2
Selina Natschke <natschke.selina@gmail.com>
2024-04-29 18:16:23 +0200

    added decoration

--- game/blender/symbolHP/SybolHP-normal.gd
+++ game/blender/symbolHP/SybolHP-normal.gd
@@ -4,6 +4,7 @@ onready var hp_glowing: Node = get_parent().get_node("SybolHP-leuchtend")
 onready var door_object: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolHPnormal")
 onready var door_object_glowing: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolHPleuchtend")
 onready var door_object_glowing2: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolTOGleuchtend")
+onready var door_object_glowing3: Node = get_node("/root/Main/MainRoom/Doorway/door/Symbol3leuchtend")
 onready var door: Node = get_node("/root/Main/MainRoom/Doorway/door")
 
 func _on_Area_input_event(camera, event, position, normal, shape_idx):
@@ -15,6 +16,6 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 			door_object.visible = false
 			door_object_glowing.visible = true
 			
-		if door_object_glowing.visible == true and door_object_glowing2.visible == true:
+		if door_object_glowing.visible == true and door_object_glowing2.visible == true and door_object_glowing3.visible:
 			door.translation = Vector3(16.604, 4.309, 20.711)
 			door.rotation_degrees = Vector3(0, 80, 0)
--- game/blender/symbolTOG/SymbolToG-normal.gd
+++ game/blender/symbolTOG/SymbolToG-normal.gd
@@ -4,6 +4,7 @@ onready var object_to_hide: Node = get_parent().get_node("SymbolToG-leuchtend")
 onready var door_object: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolTOGnormal")
 onready var door_object_glowing: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolTOGleuchtend")
 onready var door_object_glowing2: Node = get_node("/root/Main/MainRoom/Doorway/door/SymbolHPleuchtend")
+onready var door_object_glowing3: Node = get_node("/root/Main/MainRoom/Doorway/door/Symbol3leuchtend")
 onready var door: Node = get_node("/root/Main/MainRoom/Doorway/door")
 
 
@@ -16,7 +17,7 @@ func _on_Area_input_event(camera, event, position, normal, shape_idx):
 			door_object.visible = false
 			door_object_glowing.visible = true
 			
-			if door_object_glowing.visible and door_object_glowing2.visible:
+			if door_object_glowing.visible and door_object_glowing2.visible and door_object_glowing3.visible:
 				door.translation = Vector3(16.604, 4.309, 20.711)
 				door.rotation_degrees = Vector3(0, 80, 0)
 
