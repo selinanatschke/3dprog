aad563309dc005c34d366e36441a23835fdff599
Selina Natschke <natschke.selina@gmail.com>
2024-05-31 13:10:37 +0200

    removed fadenkreuz; made book implementation better

--- game/flying_books/flying_book.gd
+++ game/flying_books/flying_book.gd
@@ -15,28 +15,25 @@ onready var layer2: TextureRect = get_node("/root/Main/CableRollLayer/2")
 var velocity = Vector3.ZERO
 
 func _ready():
-	if GlobalVariables.books_flying:
-		add_to_group("mob")
+	add_to_group("mob")
 	# Initialize the layerArray after all onready variables are initialized
 
 func _physics_process(_delta):
-	if GlobalVariables.books_flying:
-		move_and_slide(velocity)
+	move_and_slide(velocity)
 	
 # We will call this function from the Main scene.
 func initialize(start_position, player_position):
-	if GlobalVariables.books_flying:
-		# We position the mob and turn it so that it looks at the player.
-		look_at_from_position(start_position, player_position, Vector3.UP)
-		# And rotate it randomly so it doesn't move exactly toward the player.
-		rotate_y(rand_range(-PI / 4, PI / 4)) 
-		
-		# We calculate a random speed.
-		var random_speed = rand_range(min_speed, max_speed)
-		# We calculate a forward velocity that represents the speed.
-		velocity = Vector3.FORWARD * random_speed
-		# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
-		velocity = velocity.rotated(Vector3.UP, rotation.y)
+	# We position the mob and turn it so that it looks at the player.
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	# And rotate it randomly so it doesn't move exactly toward the player.
+	rotate_y(rand_range(-PI / 4, PI / 4)) 
+	
+	# We calculate a random speed.
+	var random_speed = rand_range(min_speed, max_speed)
+	# We calculate a forward velocity that represents the speed.
+	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the vector based on the mob's Y rotation to move in the direction it's looking.
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
 
 func _on_VisibilityNotifier_screen_exited():
 	queue_free()
